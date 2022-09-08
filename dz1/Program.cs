63f0650c99a118f9d5bbfd8d2ca5632fc53770cc
кристина <kristytitova@mail.ru>
2022-09-08 12:30:24 +0300

    Задача 1

---
+++ dz1/Program.cs
@@ -0,0 +1,10 @@
+﻿string NaturalNum(int n)
+     {
+         if (n == 1)
+     {
+         return "1";
+     }
+        return n + " " + NaturalNum(n - 1);
+     }
+
+ Console.WriteLine(NaturalNum(10));
